03666969635cca75b249e3574f7dc13b5dddff4a
Priya Varshney <priyavarshney081003@gmail.com>
2023-01-27 23:20:37 +0530

    Create _04_InsertAtPosition.c

---
+++ _07_LinkedList/1_Singly Linked List/_04_InsertAtPosition.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+void LL_Traversal(struct node* head){
+    struct node* p = head;
+    while(p!=NULL){
+        printf("%d\n", p->data);
+        p=p->next;
+    }
+}
+
+void InsertAtPosition(struct node* head, int pos, int data){
+    struct node* ptr = (struct node*)malloc(sizeof(struct node));
+    ptr->data = data;
+    ptr->next = NULL;
+    pos--;
+    struct node* p = head;
+    while(pos!=1){
+        p = p->next;
+        pos--;
+    }
+    ptr->next = p->next;
+    p->next = ptr;
+}
+
+int main(){
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+    head->data = 1;
+    head->next = NULL;
+
+    struct node* first = (struct node*)malloc(sizeof(struct node));
+    head->next = first;
+    first->data = 2;
+    first->next = NULL;
+
+    LL_Traversal(head);
+    InsertAtPosition(head, 2, 45);
+    LL_Traversal(head);
+
+    return 0;
+}
