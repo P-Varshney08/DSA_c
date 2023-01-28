c67b5eb668f4502f5a5e8fdaa719612b296209f9
Priya Varshney <priyavarshney081003@gmail.com>
2023-01-28 21:38:49 +0530

    Create _06_DeleteFirstNode.c

---
+++ _07_LinkedList/2_Doubly Linked List/_06_DeleteFirstNode.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    struct node* prev;
+    int data;
+    struct node* next;
+};
+
+void llTraversal(struct node* head){
+    struct node* p = head;
+    while(p!=NULL){
+        printf("%d\n", p->data);
+        p=p->next;
+    }
+}
+
+struct node* DeleteFirstNode(struct node* head){
+    struct node* p = head->next;
+    p->prev = NULL;
+    free(head);
+    return p;
+}
+
+int main(){
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+    struct node* second = (struct node*)malloc(sizeof(struct node));
+    struct node* third = (struct node*)malloc(sizeof(struct node));
+    struct node* fourth = (struct node*)malloc(sizeof(struct node));
+
+    head->prev=NULL;
+    head->data=1;
+    head->next = second;
+    
+    second->prev=head;
+    second->data=43;
+    second->next = third;
+
+    third->prev=second;
+    third->data=21;
+    third->next = fourth;
+
+    fourth->prev=third;
+    fourth->data=67;
+    fourth->next = NULL;
+
+    llTraversal(head);
+    head = DeleteFirstNode(head);
+    llTraversal(head);
+    
+    return 0;
+}
