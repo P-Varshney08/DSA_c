d52204056ac137277d12567096e4f2a6ad6a5c2f
Priya Varshney <priyavarshney081003@gmail.com>
2023-02-05 00:23:36 +0530

    Create _02_InsertAtEnd.c

---
+++ _07_LinkedList/3_Circular Linked List/Circular Doubly Linked List/_02_InsertAtEnd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    struct node* prev;
+    int data;
+    struct node* next;
+};
+
+struct node* addToEmpty(int data){
+    struct node* p = (struct node *)malloc(sizeof(struct node));
+    p->prev=p;
+    p->data=data;
+    p->next=p;
+    return p;
+}
+
+void llTraversal(struct node *head){
+    struct node *p = head;
+    while (p->next != head){
+        printf("Element : %d\n", p->data);
+        p = p->next;
+    }
+    printf("Element : %d\n", p->data);
+}
+
+struct node *InsertAtEnd(struct node *head, int data){
+    struct node *tail = head;
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = data;
+    while (tail->next != head){
+        tail = tail->next;
+    }
+    ptr->next = tail->next;
+    ptr->prev = tail;
+    tail->next=ptr;
+    tail->next->prev = ptr;
+    return head;
+}
+
+int main(){
+    struct node* head = (struct node *)malloc(sizeof(struct node));
+    struct node* tail = (struct node *)malloc(sizeof(struct node));
+
+    head->prev = tail;
+    head->data=25;
+    head->next=tail;
+
+    tail->prev = head;
+    tail->data=56;
+    tail->next=head;
+    
+    // head = addToEmpty(25);
+    printf("Before :\n");
+    llTraversal(head);
+    printf("After :\n");
+    head = InsertAtEnd(head, 10);
+    llTraversal(head);
+    return 0;
+}
